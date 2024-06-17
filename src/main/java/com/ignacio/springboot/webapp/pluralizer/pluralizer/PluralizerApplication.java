245da781bcdc37ea3db48e550ea8329019ad417a
ipi <ignaciogastonpi@gmail.com>
2024-06-16 23:53:19 -0400

    first commit

---
+++ src/main/java/com/ignacio/springboot/webapp/pluralizer/pluralizer/PluralizerApplication.java
@@ -0,0 +1,13 @@
+package com.ignacio.springboot.webapp.pluralizer.pluralizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ignacio/springboot/webapp/pluralizer/pluralizer/PluralizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignacio.springboot.webapp.pluralizer.pluralizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluralizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
